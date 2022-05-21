e66339f01f435eeacfa38283234ba77e8341589c
mattmeyerink <meyerink@umich.edu>
2022-05-21 13:59:09 -0400

    add skeleton for constant timeout prevention script

---
+++ autoPartyMachine.py
@@ -0,0 +1,47 @@
+import time
+import pyautogui
+from pynput.mouse import Listener
+
+timeoutInterval = 10
+timeRemaining = timeoutInterval
+
+def resetTimer():
+    global timeRemaining
+    timeRemaining = timeoutInterval
+
+def on_move(x, y):
+    resetTimer()
+
+def on_click(x, y, button, pressed):
+    resetTimer()
+
+def on_scroll(x, y, dx, dy):
+    resetTimer()
+
+def printIntroMessage():
+    print()
+    print('Welcome to the auto party machine! We will generate a party any time the energy starts to dip!')
+    print()
+    print('Control + C to quit.')
+
+def printExitMessage():
+    print()
+    print('Every party needs to end eventually. Thanks for partying with us!')
+    print()
+
+printIntroMessage()
+
+listener = Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
+listener.start()
+
+try:
+    while True:
+        if (timeRemaining <= 0):
+            pyautogui.move(200, 200, 1)
+            pyautogui.move(-200, -200, 1)
+        else:
+            timeRemaining -= 1
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    printExitMessage()
