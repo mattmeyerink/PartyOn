c0b2288a91201edd4facdae1fb617068169a26c5
mattmeyerink <meyerink@umich.edu>
2022-05-21 12:43:26 -0400

    add finishing touches for continous movement loop

--- keepThePartyRolling.py
+++ keepThePartyRolling.py
@@ -1,12 +1,36 @@
 import pyautogui
 import time
 
-while True:
-    pyautogui.move(200, 0, 1)
-    time.sleep(30)
-    pyautogui.move(0, -200, 1)
-    time.sleep(30)
-    pyautogui.move(-200, 1)
-    time.sleep(30)
-    pyautogui.move(0, 200, 1)
-    time.sleep(30)
+"""
+Move the mouse in a clockwise square continuously in 1 second movements every 30 seconds.
+"""
+def keepThePartyRolling():
+    printIntroMessage()
+
+    try:
+        while True:
+            pyautogui.move(200, 0, 1)
+            time.sleep(30)
+            pyautogui.move(0, 200, 1)
+            time.sleep(30)
+            pyautogui.move(-200, 0, 1)
+            time.sleep(30)
+            pyautogui.move(0, -200, 1)
+            time.sleep(30)
+
+    except KeyboardInterrupt:
+        printExitMessage()
+
+def printIntroMessage():
+    print()
+    print('Lets keep the party rolling!')
+    print()
+    print('Control + C to quit.')
+    print()
+
+def printExitMessage():
+    print()
+    print('Stopping now! Take it from here!')
+    print()
+
+keepThePartyRolling()
