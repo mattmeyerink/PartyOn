651ceb00dc522ab7835ba718cf0f8b9e502b6b2a
mattmeyerink <meyerink@umich.edu>
2022-05-23 13:12:03 -0400

    add more efficient minimalist way to keep screen active

---
+++ smallPartyMachine.py
@@ -0,0 +1,27 @@
+"""
+Asks the user to input a float to serve as a timeout interval.
+Moves the mouse one pixel diagonally back and fourth once per the timeout interval.
+"""
+
+import time
+import pyautogui
+
+try: 
+    print()
+    timeoutInterval = float(input('Enter the interval between movements in minutes (Ex: 5.5): ')) * 60
+    print()
+except ValueError:
+    print()
+    print('That wasn\'t a number you dongus. Please cooporate next time')
+    print()
+    exit()
+
+try: 
+    while True:
+        pyautogui.move(1, 1)
+        pyautogui.move(-1, -1)
+        time.sleep(timeoutInterval)
+except KeyboardInterrupt:
+    print()
+    print('Small party coming to a close. Over and out. Have nice day!')
+    print()
